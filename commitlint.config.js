d78226f93f473e224b097b9f80ec63e71d1777ad
highskore <luka@racki.dev>
2024-08-14 04:19:33 +0200

    chore: lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,10 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'chore',
-        'style',
-        'refactor',
-        'ci',
-        'test',
-        'revert',
-        'release'
-      ],
+      "always",
+      ["feat", "fix", "docs", "chore", "style", "refactor", "ci", "test", "revert", "release"],
     ],
   },
 };
